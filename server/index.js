1bb3ffb62c6a9fbe3e5b0f891a0f41792f6a5991
Jacob Shalabi <jshalabi03@gmail.com>
2023-03-27 22:19:19 -0500

    initialized npm project for server

--- server/index.js
+++ server/index.js
@@ -1,50 +1,33 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-app.use(bodyParser.json())
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
 
+app.post("/answer", (req, res) => {
+  console.log(req.body);
+  console.log(req.body.answer);
+});
 
+app.get("/question", (req, res) => {
+  console.log("Getting question");
+  const question1 = {
+    id: 1,
+    text: "What is the capital of France?",
+    options: ["A: Paris", "B: Berlin", "C: Madrid", "D: London"],
+    answer: "A",
+  };
+  const question2 = {
+    id: 2,
+    text: "What is the capital of Germany?",
+    options: ["A: Paris", "B: Berlin", "C: Madrid", "D: London"],
+    answer: "B",
+  };
+  const chosen = Math.random() > 0.5 ? question1 : question2;
+  res.json(chosen);
+});
 
-// Set up a route to handle POST requests for submitting a student's answer
-app.post('/answer', (req, res) => {
-    // Print in console the answer submitted by the student
-    console.log(req.body);
-    console.log(req.body.answer)
-
-
-
-  
-    // Process the answer and send a response back to the client
-    // ...
-
-  });
-
-  // Set up a route to handle GET requests for fetching a question
-    // app.get('/question', (req, res) => {
-    // // Extract the question ID from the request query
-    // const { questionId } = req.query;
-
-    // // Fetch the question and send a response back to the client
-    // // ...
-    // });
-    
-
-    app.get('/question',  (req, res) => {
-        console.log("Getting question")
-        const question = {
-          id: 1,
-          text: 'What is the capital of France?',
-          options: ['A: Paris', 'B: Berlin', 'C: Madrid', 'D: London'],
-          answer: 'A'
-        };
-        res.json(question);
-    });
-
-
-  
-  // Start the server and listen on a specific port
-
-const port =  3000;
+// Start the server and listen on a specific port
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
